149044b48a8fc02a731cc8a6066c3079f18cdee0
Fikriye Barut <fikriye.barut9@gmail.com>
2023-02-10 22:26:19 +0300

    section overriding

---
+++ overriding2/src/overriding2/BaseKrediManager.java
@@ -0,0 +1,7 @@
+package overriding2;
+
+public class BaseKrediManager {
+	public double hesapla(double tutar) {
+		return tutar * 1.18;
+	}
+}
---
+++ overriding2/src/overriding2/OgrenciKrediManager.java
@@ -0,0 +1,7 @@
+package overriding2;
+
+public class OgrenciKrediManager extends BaseKrediManager {
+	public double hesapla(double tutar) {
+		return tutar * 1.10;
+	}
+}
